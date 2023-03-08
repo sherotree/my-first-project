5c495cec9e6752f813dbf1c2c983839fbaa91f4f
sherotree <wb-wangyang@cai-inc.com>
2023-03-08 09:28:44 +0800

    feat: 设置主题色

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Button } from 'antd';
+import { Button, ConfigProvider } from 'antd';
 import MyButton from './button';
 import MyButton2 from './my-button2';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './view/home';
 import Foo from './view/foo';
 import 'antd/dist/reset.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span>现在的环境： {process.env.NODE_ENV}</span>
-        <MyButton2 />
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button type="primary">按钮</Button>
-        <MyButton />
-        <div className='text-3xl font-bold red'>tailwind</div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <img src={process.env.PUBLIC_URL + 'favicon.ico'} alt='logo'></img>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <ConfigProvider theme={{token: {
+      colorPrimary: '#00b96b'
+    }}}>
+      <div className="App">
+        <header className="App-header">
+          <span>现在的环境： {process.env.NODE_ENV}</span>
+          <MyButton2 />
+          <img src={logo} className="App-logo" alt="logo" />
+          <Button type="primary">按钮</Button>
+          <MyButton />
+          <div className='text-3xl font-bold red'>tailwind</div>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <img src={process.env.PUBLIC_URL + 'favicon.ico'} alt='logo'></img>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
 
-      </header>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='foo' element={<Foo />}/>
-      </Routes>
-    </div>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='foo' element={<Foo />}/>
+        </Routes>
+      </div>
+    </ConfigProvider>
   );
 }
 
